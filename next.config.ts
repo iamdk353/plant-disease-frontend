4bfa0c02abed8f087d81dec340355f7c4ca95de6
Dhanush <iamdk353@gmail.com>
2026-04-03 12:18:28 +0530

    working set

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://129.159.239.239:8000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
